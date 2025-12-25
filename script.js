af8b51bfdc089160c27f920b19c1c85fb93c8941
Asseem Sony <58298484+AseeeSoni888@users.noreply.github.com>
2025-12-25 18:47:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function blowCandles() {
+  const flames = document.querySelectorAll(".candle");
+
+  flames.forEach((candle, index) => {
+    setTimeout(() => {
+      candle.classList.add("blown");
+    }, index * 60); // staggered blow effect
+  });
+}
